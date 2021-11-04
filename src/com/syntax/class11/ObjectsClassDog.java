a9256885d9655655348613cc911b60265e52bdcc
jimmycastro <ndrecastro@gmail.com>
2021-11-03 22:58:34 -0400

    class 11

---
+++ src/com/syntax/class11/ObjectsClassDog.java
@@ -0,0 +1,22 @@
+package com.syntax.class11;
+
+public class ObjectsClassDog {
+
+	String name;
+	String breed;
+	String color;
+	double height;
+	int age;
+	char gender;
+		void run() {
+			System.out.println(name+" runs at 30mph");
+		}
+		void eats() {
+			System.out.println(name+" eats 3 times a day");
+		}
+		void howls() {
+			System.out.println(name+ " howls every night");
+		}
+		}
+
+
---
+++ src/com/syntax/class11/Phone.java
@@ -0,0 +1,21 @@
+package com.syntax.class11;
+
+public class Phone {
+
+	String brand;
+	String model;
+	String color;
+	String size;
+	int memory;
+	int year;
+	
+	void siri() {
+		System.out.println(brand+ " contains Siri");
+	}
+	void snake () {
+		System.out.println(brand+ " contains the Snake Game");
+	}
+	void alexa() {
+		System.out.println(brand+ " contains Alexa");
+	}
+}
